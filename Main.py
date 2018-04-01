e7bfbe0ca3d04b7e9362bfd10cbf020fae0ff7bb
E-reppchen <37449930+E-reppchen@users.noreply.github.com>
2018-04-01 11:46:30 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,16 @@
-import numpy
+import numpy as np
 import picamera
 import time
 
 if __name__ == "__main__":
 	main()
+
+def timeMatrix(currentArray, previousArray):
+	#time matrix is the current matrix - the previous matrix
+	timeMatrix = currentArray - previousArray
+	#make current matrix the next previous matrix
+	previousArray = currentArray
+	return (timeMatrix, previousArray)
 
 def main():	
 	#camera used referenced as camera
@@ -18,3 +25,10 @@ def main():
 	for i, image in enumerate(camera.capture_continuous(arrayFormat, format = 'bgr', use_video_port = True)):
 		#numpy version of the image
 		inputArray = image.array
+		#dimension of the array
+		dimensions = inputArray.shape
+		#we make an empty array of the same dimensions to start off with
+		if i == 0:
+			previousArray = np.zeros(dimensions)
+		#we might have to declare timeArray beforehand (timeArray = np.zeros(dimensions))
+		timeArray, previousArray = timeMatrix(currentArray, previousArray)
