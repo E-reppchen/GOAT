c143881f52ffa6457d228dca227b50569ad807a6
E-reppchen <37449930+E-reppchen@users.noreply.github.com>
2018-04-01 11:12:54 -0500

    Update timeMatrixTester.py

--- timeMatrixTester.py
+++ timeMatrixTester.py
@@ -1,8 +1,21 @@
-import numpy
+import numpy as np
 
-def timeMatrix(currentArray, timeStep):
-  if timeStep == 0:
-    previousArray = np.zeros(currentArray.shape)
-  timeMatrix = currentArray - previousArray
-  previousArray = currentArray
-  return timeMatrix
+def timeMatrix(currentArray, previousArray):
+	timeMatrix = currentArray - previousArray
+	print(previousArray)
+	previousArray = currentArray
+	print(timeMatrix)
+	return previousArray
+
+def main():
+	#previous array would be declared in an if statement in main method at beginning of program
+	#if timeStep==0:
+	#	previousArray = np.zeros(currentArray.shape)
+	currentArray = np.array([[1,2], [3,4]])
+	previousArray = np.zeros(currentArray.shape)
+	previousArray = timeMatrix(currentArray, previousArray)
+	currentArray = np.array([[2,3], [4,5]])
+	previousArray = timeMatrix(currentArray, previousArray)
+
+if __name__ == "__main__":
+    main()
