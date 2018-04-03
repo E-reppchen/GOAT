54f6b7170823a2884f11ddfb42320a170c8b1887
E-reppchen <37449930+E-reppchen@users.noreply.github.com>
2018-04-02 20:41:02 -0500

    Update DifferenceMatrix.py

--- DifferenceMatrix.py
+++ DifferenceMatrix.py
@@ -1,2 +1,6 @@
-def differenceMatrix():
-  
+def differenceMatrix(dimensions, inputArray):
+  x, y, z = dimensions
+  point = [2, 2, 2]
+  diffArray = np.zeros(dimensions)
+  for i in range(point[2], z+1):
+    
