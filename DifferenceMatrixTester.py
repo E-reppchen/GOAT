013e359b9ebf053610627248667c3c02ffbabc53
E-reppchen <37449930+E-reppchen@users.noreply.github.com>
2018-04-03 18:50:22 -0500

    Update and rename DifferenceMatrix.py to DifferenceMatrixTester.py

--- DifferenceMatrixTester.py
+++ DifferenceMatrixTester.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def differenceMatrix(dimensions, inputArray, n):
   x, y, z = dimensions
   n = n-1
@@ -5,11 +7,12 @@ def differenceMatrix(dimensions, inputArray, n):
   point2 = [n, n, n]
   diffArray = np.zeros(dimensions)
   timeUsedArray = np.zeros(dimensions)
-  for i in range(point[2], z+1):
+  localAv = 0
+  for i in range(point[2], z):
     point2[1] = n
-    for i in range(point[1], y+1):
+    for i in range(point[1], y):
       point2[0] = n
-      for i in range(point[0], x+1):
+      for i in range(point[0], x):
         for j in range(point2[2] - n, point[2]+1): 
           for k in range(point2[1] - n, point[1]+1):
             for l in range(point2[0] - n, point[0]+1):
@@ -29,6 +32,12 @@ def differenceMatrix(dimensions, inputArray, n):
   for i in range(z):
     for j in range(y):
       for k in range(x):
-        diffArray[i][j][k] = diffArray[i][j][k]/timeUsedArray[i][j][k]
+        diffArray[k][j][i] = diffArray[k][j][i]/timeUsedArray[k][j][i]
   
   return diffArray
+
+if __name__ == "__main__":
+  inputArray = np.random.rand(64, 324, 3)
+  dimensions = inputArray.shape
+  diffArray = differenceMatrix(dimensions, inputArray, 3)
+  print(diffArray)
