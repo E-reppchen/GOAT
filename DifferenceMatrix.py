69ec658dffbc8b7a8e043f052d92a4e8eb70a028
E-reppchen <37449930+E-reppchen@users.noreply.github.com>
2018-04-02 20:49:31 -0500

    Update DifferenceMatrix.py

--- DifferenceMatrix.py
+++ DifferenceMatrix.py
@@ -1,6 +1,12 @@
-def differenceMatrix(dimensions, inputArray):
+def differenceMatrix(dimensions, inputArray, n):
   x, y, z = dimensions
-  point = [2, 2, 2]
+  filterSize = n
+  point = [n, n, n]
   diffArray = np.zeros(dimensions)
   for i in range(point[2], z+1):
-    
+    for i in range(point[1], y+1):
+      for i in range(point[0], x+1):
+        for j in range(point[2] - n, point[2] + 1):
+          for j in range(point[1] - n, point[1] + 1):
+            for j in range(point[0] - n, point[0] +1):
+              localAv += inputArray[point[0]][point[1]][point[2]]
