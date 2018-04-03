dfb2fffa7cf2d33201b946be9f78324410f25c79
E-reppchen <37449930+E-reppchen@users.noreply.github.com>
2018-04-02 21:52:09 -0500

    Update DifferenceMatrix.py

--- DifferenceMatrix.py
+++ DifferenceMatrix.py
@@ -1,19 +1,20 @@
 def differenceMatrix(dimensions, inputArray, n):
   x, y, z = dimensions
-  filterSize = n
+  n = n-1
   point = [n, n, n]
   diffArray = np.zeros(dimensions)
   timeUsedArray = np.zeros(dimensions)
   for i in range(point[2], z+1):
     for i in range(point[1], y+1):
       for i in range(point[0], x+1):
-        for j in range(point[2] - n, point[2] + 1):
-          for j in range(point[1] - n, point[1] + 1):
-            for j in range(point[0] - n, point[0] +1):
+        for j in range(point[2] - n, point[2]+1): 
+          for j in range(point[1] - n, point[1]+1):
+            for j in range(point[0] - n, point[0]+1):
               localAv += inputArray[point[0]][point[1]][point[2]]
-        for j in range(point[2] - n, point[2] + 1):
-          for j in range(point[1] - n, point[1] + 1):
-            for j in range(point[0] - n, point[0] +1):
+        localAv = localAv/(n*n*n)
+        for j in range(point[2] - n, point[2]+1):
+          for j in range(point[1] - n, point[1]+1):
+            for j in range(point[0] - n, point[0]+1):
               percentDiff = (abs(inputArray[point[0]][point[1]][point[2]] - localAv))/localAv*100
               diffArray[point[0]][point[1]][point[2]] += percentDiff
               timeUsedArray[point[0]][point[1]][point[2]] += 1
