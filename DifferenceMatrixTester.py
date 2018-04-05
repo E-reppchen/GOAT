62f28caefb9734e7ce2cb2ced4b31afda6d75857
E-reppchen <37449930+E-reppchen@users.noreply.github.com>
2018-04-04 20:02:15 -0500

    Update DifferenceMatrixTester.py

--- DifferenceMatrixTester.py
+++ DifferenceMatrixTester.py
@@ -4,31 +4,24 @@ def differenceMatrix(dimensions, inputArray, n):
   x, y, z = dimensions
   n = n-1
   point = [n, n, n]
-  point2 = [n, n, n]
   diffArray = np.zeros(dimensions)
   timeUsedArray = np.zeros(dimensions)
   localAv = 0
-  for i in range(point[2], z+1):
-    point2[1] = n
-    point2[0] = n
-    for i in range(point[1], y+1):
-      point2[0] = n
-      for i in range(point[0], x+1):
-        for j in range(point2[2] - n, point[2]+1): 
-          for k in range(point2[1] - n, point[1]+1):
-            for l in range(point2[0] - n, point[0]+1):
+  for c in range(point[2], z+1):
+    for b in range(point[1], y+1):
+      for a in range(point[0], x+1):
+        for j in range(c - n, c+1): 
+          for k in range(b - n, b+1):
+            for l in range(a - n, a+1):
               localAv += inputArray[l][k][j]
         localAv = localAv/(n*n*n)
-        for j in range(point2[2] - n, point2[2]+1):
-          for k in range(point2[1] - n, point2[1]+1):
-            for l in range(point2[0] - n, point2[0]+1):
+        for j in range(c - n, c+1):
+          for k in range(b - n, b+1):
+            for l in range(a - n, a+1):
               percentDiff = (abs(inputArray[l][k][j] - localAv))/localAv*100.00        
               diffArray[l][k][j] += percentDiff
               timeUsedArray[l][k][j] += 1
-        point2[0] += 1
         localAv = 0
-      point2[1] += 1 
-    point2[2] += 1
     
   for i in range(z+1):
     for j in range(y+1):
